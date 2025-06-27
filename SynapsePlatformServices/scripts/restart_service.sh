bd54798e7181841e4c79bf527f3d1c99ad3699b1
Jenkins <jenkins@ip-10-0-1-139.ap-south-1.compute.internal>
2025-06-27 09:08:14 +0100

    Release v1.24

--- SynapsePlatformServices/scripts/restart_service.sh
+++
@@ -1,2 +0,0 @@
-sudo systemctl restart synapseplatformservices.service
-sudo systemctl restart nginx
--- SynapsePlatformServices/scripts/stop_service.sh
+++
@@ -1,3 +0,0 @@
-sudo systemctl stop synapseplatformservices.service
-sudo systemctl stop nginx
-sudo rm -rf /var/www/synapse_platform_services/*
